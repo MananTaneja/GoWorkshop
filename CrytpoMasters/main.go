356657bca8305f4c88dc284075bc710d77a62127
MananTaneja <manantaneja007@gmail.com>
2025-01-29 02:28:25 +0530

    feat: using go routines

--- CrytpoMasters/main.go
+++ CrytpoMasters/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"manantaneja.com/go/crypto-masters/api"
 )
 
 func main() {
-	cyptoRate, err := api.GetRate("BTC")
+	var wg sync.WaitGroup // this is used to keep track of go routines
+
+	currencies := []string{"BTC", "ETH", "BCH"}
+
+	for _, currency := range currencies {
+		wg.Add(1)
+
+		// anonymous lambda function
+		go func(currency string) {
+			getCurrency(currency)
+			wg.Done() // go support closure
+		}(currency)
+	}
+	wg.Wait()
+}
+
+func getCurrency(currencyName string) {
+	cyptoRate, err := api.GetRate(currencyName)
 
 	if err != nil {
 		fmt.Println(err)
